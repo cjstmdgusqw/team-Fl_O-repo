9e37885289cc31c20d91567f7cd46d6bdf04c93d
koon <cjstmdgusqw@gmail.com>
2022-09-27 14:28:38 +0900

    init

--- Flo_starts/models.py
+++ Flo_starts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class AcessLog(models.Model):
+    class Meta:
+        db_table = "my_data"
+
+    created_at = models.DateTimeField("접속 시간", auto_now_add=True)
+    location = models.CharField("접속 경로",max_length=50)
